69a4a23b676ff03e8da57dadc394a0f0ab59bf05
masonishee <mishee2010@gmail.com>
2023-11-05 10:13:20 -0600

    Add workout model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_01_004429) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_05_161127) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -36,6 +36,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_01_004429) do
     t.string "email"
     t.string "password_digest"
     t.string "image_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "workouts", force: :cascade do |t|
+    t.string "name"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
